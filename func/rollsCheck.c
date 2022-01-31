f73032ed76bb08ae0e47b16035432a8c37cdc0c2
xactoes <ecallawnivek@gmail.com>
2022-01-31 15:07:33 -0500

    almost fixed the bug with bonus tally

--- func/rollsCheck.c
+++ func/rollsCheck.c
@@ -13,7 +13,7 @@ unsigned int rollsCheck(){
     //else if game is still going
     else if(viewGame == 1 && viewEnd == 0){
         //if scorecard has changed or the scoreBuf indicates that a bonus was chosen
-        if(scorecardChangeA != scorecardChangeB || (turn - ( (scorecard[7]/100) - (bonusTally - 1) ) ) == turn){
+        if(scorecardChangeA != scorecardChangeB || (scorecard[7] != 255 && (turn - scorecard[7]/100) < 14) ){
             scoreBuf == 0;
             //report that scorecard has been selected so the turn and rolls needs to be reset
             return 255;
